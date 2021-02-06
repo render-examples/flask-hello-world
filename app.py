0a35f9f97a32ad71d0331d31c9353b6120da7ca4
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2021-02-06 15:02:27 -0300

    Create utils and fix issue with num of operations

--- app.py
+++ app.py
@@ -3,137 +3,7 @@ import yfinance as yf
 from flask import Response, request, jsonify
 from markupsafe import escape
 from datetime import date, datetime, timedelta
-import numpy as np
-import math
-import pandas as pd
-
-# Create a function to round any number to the smallest multiple of 100
-def round_down(x):
-    return int(math.floor(x / 100.0)) * 100
-
-
-def rsi(data, column, window=2):
-
-    data = data.copy()
-
-    # Establish gains and losses for each day
-    data["Variation"] = data[column].diff()
-    data = data[1:]
-    data["Gain"] = np.where(data["Variation"] > 0, data["Variation"], 0)
-    data["Loss"] = np.where(data["Variation"] < 0, data["Variation"], 0)
-
-    # Calculate simple averages so we can initialize the classic averages
-    simple_avg_gain = data["Gain"].rolling(window).mean()
-    simple_avg_loss = data["Loss"].abs().rolling(window).mean()
-    classic_avg_gain = simple_avg_gain.copy()
-    classic_avg_loss = simple_avg_loss.copy()
-
-    for i in range(window, len(classic_avg_gain)):
-        classic_avg_gain[i] = (
-            classic_avg_gain[i - 1] * (window - 1) + data["Gain"].iloc[i]
-        ) / window
-        classic_avg_loss[i] = (
-            classic_avg_loss[i - 1] * (window - 1) + data["Loss"].abs().iloc[i]
-        ) / window
-
-    # Calculate the RSI
-    RS = classic_avg_gain / classic_avg_loss
-    RSI = 100 - (100 / (1 + RS))
-    return RSI
-
-
-def strategy_points(data, rsi_parameter=None):
-    rsi_parameter = 30 if rsi_parameter is None else rsi_parameter
-
-    data["Target1"] = data["High"].shift(1)
-    data["Target2"] = data["High"].shift(2)
-    data["Target"] = data[["Target1", "Target2"]].max(axis=1)
-
-    # We don't need them anymore
-    data.drop(columns=["Target1", "Target2"], inplace=True)
-
-    # Define exact buy price
-    data["Buy Price"] = np.where(data["IFR2"] <= rsi_parameter, data["Close"], np.nan)
-
-    # Define exact sell price
-    data["Sell Price"] = np.where(
-        data["High"] > data["Target"],
-        np.where(data["Open"] > data["Target"], data["Open"], data["Target"]),
-        np.nan,
-    )
-    return data
-
-
-def backtest_algorithm(data, initial_capital=10000):
-
-    # List with the total capital after every operation
-    total_capital = [initial_capital]
-
-    # List with profits for every operation. We initialize with 0 so
-    # both lists have the same size
-    all_profits = [0]
-
-    ongoing = False
-
-    for i in range(0, len(data)):
-        if ongoing == True:
-            if ~(np.isnan(data["Sell Price"][i])):
-
-                # Define exit point and total profit
-                exit = data["Sell Price"][i]
-                profit = shares * (exit - entry)
-
-                # Append profit to list and create a new entry with the capital
-                # after the operation is complete
-                all_profits += [profit]
-                current_capital = total_capital[
-                    -1
-                ]  # current capital is the last entry in the list
-                total_capital += [current_capital + profit]
-                ongoing = False
-
-        else:
-            if ~(np.isnan(data["Buy Price"][i])):
-                entry = data["Buy Price"][i]
-                shares = round_down(initial_capital / entry)
-                ongoing = True
-
-    return all_profits, total_capital
-
-
-def get_drawdown(data, column="Adj Close"):
-    data["Max"] = data[column].cummax()
-    data["Delta"] = data["Max"] - data[column]
-    data["Drawdown"] = 100 * (data["Delta"] / data["Max"])
-    max_drawdown = data["Drawdown"].max()
-    return max_drawdown
-
-
-def strategy_test(all_profits, total_capital):
-    num_operations = len(all_profits) - 1
-    gains = sum(x >= 0 for x in all_profits)
-    pct_gains = 100 * (gains / num_operations)
-    losses = num_operations - gains
-    pct_losses = 100 - pct_gains
-    total_profit = sum(all_profits)
-
-    # The first value entry in total_capital is the initial capital
-    pct_profit = (total_profit / total_capital[0]) * 100
-
-    # Compute drawdown
-    total_capital = pd.DataFrame(data=total_capital, columns=["total_capital"])
-    drawdown = get_drawdown(data=total_capital, column="total_capital")
-
-    return {
-        "num_operations": num_operations,
-        "gains": int(gains),
-        "pct_gains": pct_gains.round(),
-        "losses": int(losses),
-        "pct_losses": pct_losses.round(),
-        "total_profit": total_profit,
-        "pct_profit": pct_profit,
-        "drawdown": drawdown,
-    }
+from utils import rsi, strategy_points, strategy_test, backtest_algorithm
 
 
 app = Flask(__name__)
@@ -141,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!!"
+    return "Hello, World!"
 
 
 @app.route("/price/<ticker>")
---
+++ utils.py
@@ -0,0 +1,134 @@
+import numpy as np
+import math
+import pandas as pd
+
+# Create a function to round any number to the smallest multiple of 100
+def round_down(x):
+    return int(math.floor(x / 100.0)) * 100
+
+
+def rsi(data, column, window=2):
+
+    data = data.copy()
+
+    # Establish gains and losses for each day
+    data["Variation"] = data[column].diff()
+    data = data[1:]
+    data["Gain"] = np.where(data["Variation"] > 0, data["Variation"], 0)
+    data["Loss"] = np.where(data["Variation"] < 0, data["Variation"], 0)
+
+    # Calculate simple averages so we can initialize the classic averages
+    simple_avg_gain = data["Gain"].rolling(window).mean()
+    simple_avg_loss = data["Loss"].abs().rolling(window).mean()
+    classic_avg_gain = simple_avg_gain.copy()
+    classic_avg_loss = simple_avg_loss.copy()
+
+    for i in range(window, len(classic_avg_gain)):
+        classic_avg_gain[i] = (
+            classic_avg_gain[i - 1] * (window - 1) + data["Gain"].iloc[i]
+        ) / window
+        classic_avg_loss[i] = (
+            classic_avg_loss[i - 1] * (window - 1) + data["Loss"].abs().iloc[i]
+        ) / window
+
+    # Calculate the RSI
+    RS = classic_avg_gain / classic_avg_loss
+    RSI = 100 - (100 / (1 + RS))
+    return RSI
+
+
+def strategy_points(data, rsi_parameter=None):
+    rsi_parameter = 30 if rsi_parameter is None else rsi_parameter
+
+    data["Target1"] = data["High"].shift(1)
+    data["Target2"] = data["High"].shift(2)
+    data["Target"] = data[["Target1", "Target2"]].max(axis=1)
+
+    # We don't need them anymore
+    data.drop(columns=["Target1", "Target2"], inplace=True)
+
+    # Define exact buy price
+    data["Buy Price"] = np.where(data["IFR2"] <= rsi_parameter, data["Close"], np.nan)
+
+    # Define exact sell price
+    data["Sell Price"] = np.where(
+        data["High"] > data["Target"],
+        np.where(data["Open"] > data["Target"], data["Open"], data["Target"]),
+        np.nan,
+    )
+    return data
+
+
+def backtest_algorithm(data, initial_capital=10000):
+
+    # List with the total capital after every operation
+    total_capital = [initial_capital]
+
+    # List with profits for every operation. We initialize with 0 so
+    # both lists have the same size
+    all_profits = [0]
+
+    ongoing = False
+
+    shares, entry = 0, 0
+
+    for i in range(0, len(data)):
+        if ongoing == True:
+            if ~(np.isnan(data["Sell Price"][i])):
+
+                # Define exit point and total profit
+                exit = data["Sell Price"][i]
+                profit = shares * (exit - entry)
+
+                # Append profit to list and create a new entry with the capital
+                # after the operation is complete
+                all_profits += [profit]
+                current_capital = total_capital[
+                    -1
+                ]  # current capital is the last entry in the list
+                total_capital += [current_capital + profit]
+                ongoing = False
+
+        else:
+            if ~(np.isnan(data["Buy Price"][i])):
+                entry = data["Buy Price"][i]
+                shares = round_down(initial_capital / entry)
+                ongoing = True
+
+    return all_profits, total_capital
+
+
+def get_drawdown(data, column="Adj Close"):
+    data["Max"] = data[column].cummax()
+    data["Delta"] = data["Max"] - data[column]
+    data["Drawdown"] = 100 * (data["Delta"] / data["Max"])
+    max_drawdown = data["Drawdown"].max()
+    return max_drawdown
+
+
+def strategy_test(all_profits, total_capital):
+    print(all_profits)
+    gains = sum(x > 0 for x in all_profits)
+    losses = sum(x < 0 for x in all_profits)
+    num_operations = gains + losses
+    pct_gains = 100 * (gains / num_operations)
+    pct_losses = 100 - pct_gains
+    total_profit = sum(all_profits)
+
+    # The first value entry in total_capital is the initial capital
+    pct_profit = (total_profit / total_capital[0]) * 100
+
+    # Compute drawdown
+    total_capital = pd.DataFrame(data=total_capital, columns=["total_capital"])
+    drawdown = get_drawdown(data=total_capital, column="total_capital")
+
+    return {
+        "num_operations": int(num_operations),
+        "gains": int(gains),
+        "pct_gains": pct_gains.round(),
+        "losses": int(losses),
+        "pct_losses": pct_losses.round(),
+        "total_profit": total_profit,
+        "pct_profit": pct_profit,
+        "drawdown": drawdown,
+    }
