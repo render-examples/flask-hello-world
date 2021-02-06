e893646dadb6f3875767257dcd9001dbb3ebf6c1
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2021-02-06 11:04:19 -0300

    Add new endpoints

--- app.py
+++ app.py
@@ -1,6 +1,65 @@
 from flask import Flask
+import yfinance as yf
+from flask import Response, request
+from markupsafe import escape
+from datetime import date, datetime, timedelta
+import numpy as np
+
+
+def rsi(data, column, window=2):
+
+    data = data.copy()
+
+    # Establish gains and losses for each day
+    data["Variation"] = data[column].diff()
+    data = data[1:]
+    data["Gain"] = np.where(data["Variation"] > 0, data["Variation"], 0)
+    data["Loss"] = np.where(data["Variation"] < 0, data["Variation"], 0)
+
+    # Calculate simple averages so we can initialize the classic averages
+    simple_avg_gain = data["Gain"].rolling(window).mean()
+    simple_avg_loss = data["Loss"].abs().rolling(window).mean()
+    classic_avg_gain = simple_avg_gain.copy()
+    classic_avg_loss = simple_avg_loss.copy()
+
+    for i in range(window, len(classic_avg_gain)):
+        classic_avg_gain[i] = (
+            classic_avg_gain[i - 1] * (window - 1) + data["Gain"].iloc[i]
+        ) / window
+        classic_avg_loss[i] = (
+            classic_avg_loss[i - 1] * (window - 1) + data["Loss"].abs().iloc[i]
+        ) / window
+
+    # Calculate the RSI
+    RS = classic_avg_gain / classic_avg_loss
+    RSI = 100 - (100 / (1 + RS))
+    return RSI
+
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    return "Hello, World!!"
+
+
+@app.route("/price/<ticker>")
+def me_api(ticker):
+    yf_ticker = escape(ticker) + ".SA"
+    start = request.args.get("start")
+    end = request.args.get("end")
+    df = yf.download(yf_ticker, start=start, end=end).copy()[["Open", "High", "Close"]]
+    return Response(df.to_json(orient="table"), mimetype="application/json")
+
+
+@app.route("/ifr2/<ticker>")
+def ifr2(ticker):
+    yf_ticker = escape(ticker) + ".SA"
+    start = (datetime.today() - timedelta(days=50)).strftime("%Y-%m-%d")
+    end = (date.today() + timedelta(days=1)).strftime("%Y-%m-%d")
+    df = yf.download(yf_ticker, start=start, end=end).copy()[
+        ["Open", "High", "Adj Close"]
+    ]
+    ifr2_df = rsi(df, "Adj Close", 2)
+    return {"ifr2": int(round(ifr2_df[-1]))}
