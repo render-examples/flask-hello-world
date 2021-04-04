426e11dc67c7139e8504dc9ab3b00f6e0279c44d
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2021-04-04 15:14:45 -0300

    Remove duplicate values from nested objects

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ from utils import (
     strategy_test,
     backtest_algorithm,
     stochastic,
-    get_data, 
+    get_data,
     get_interval,
     get_rsi_info,
-    get_stochastic_info
+    get_stochastic_info,
 )
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
@@ -39,10 +40,7 @@ def ifr2(ticker):
     yf_ticker = escape(ticker) + ".SA"
     start, end = get_interval(50)
     df = get_data(
-        tickers=yf_ticker,
-        columns=["Open", "High", "Adj Close"],
-        start=start,
-        end=end
+        tickers=yf_ticker, columns=["Open", "High", "Adj Close"], start=start, end=end
     )
     ifr2_df = rsi(df, "Adj Close", 2)
     return {"ifr2": int(round(ifr2_df[-1]))}
@@ -56,7 +54,7 @@ def backtest_ifr2(ticker):
         tickers=yf_ticker,
         columns=["Open", "High", "Close", "Adj Close"],
         start=start,
-        end=end
+        end=end,
     )
     df["IFR2"] = rsi(df, column="Adj Close")
     entry = (
@@ -74,10 +72,7 @@ def api():
     tickers = get_ibov_tickers()
     start, end = get_interval(100)
     df = get_data(
-        tickers=tickers,
-        columns=["Open", "High", "Adj Close"],
-        start=start,
-        end=end
+        tickers=tickers, columns=["Open", "High", "Adj Close"], start=start, end=end
     )
 
     all_rsi = get_rsi_info(df, tickers)
@@ -89,13 +84,8 @@ def api():
 def bollinger_bands(ticker):
     yf_ticker = escape(ticker) + ".SA"
     start, end = get_interval(50)
-    df = get_data(
-        tickers=yf_ticker,
-        columns=["Adj Close"],
-        start=start,
-        end=end
-    )
-    
+    df = get_data(tickers=yf_ticker, columns=["Adj Close"], start=start, end=end)
+
     k = 2 if request.args.get("k") is None else float(request.args.get("k"))
     n = 20 if request.args.get("n") is None else int(request.args.get("n"))
 
@@ -110,39 +100,36 @@ def bollinger_bands(ticker):
         }
     )
 
+
 @app.route("/stochastic/<ticker>")
 def stochastic_calculation(ticker):
     yf_ticker = escape(ticker) + ".SA"
     start, end = get_interval(50)
     df = get_data(
-        tickers=yf_ticker,
-        columns=["High", "Low", "Adj Close"],
-        start=start,
-        end=end
+        tickers=yf_ticker, columns=["High", "Low", "Adj Close"], start=start, end=end
     )
     df = stochastic(df)
     return {
-        "fast_k": int(round(df["%K"][-1])), 
+        "fast_k": int(round(df["%K"][-1])),
         "fast_d": int(round(df["%D"][-1])),
         "k": int(round(df["Slow %K"][-1])),
-        "d": int(round(df["Slow %D"][-1]))
+        "d": int(round(df["Slow %D"][-1])),
     }
-    
+
+
 @app.route("/stochastic")
 def all_stochastic():
 
     tickers = get_ibov_tickers()
     start, end = get_interval(120)
     df = get_data(
-        tickers=tickers,
-        columns=["High", "Low", "Adj Close"],
-        start=start,
-        end=end
+        tickers=tickers, columns=["High", "Low", "Adj Close"], start=start, end=end
     )
-    
+
     all_stochastic = get_stochastic_info(df, tickers)
-    
+
     return jsonify(all_stochastic)
+
 
 @app.route("/static")
 def indicators():
@@ -152,19 +139,37 @@ def indicators():
         tickers=tickers,
         columns=["Open", "High", "Low", "Adj Close"],
         start=start,
-        end=end
+        end=end,
     )
-    
+
     all_rsi = get_rsi_info(df.copy(), tickers)
     all_stochastic = get_stochastic_info(df.copy(), tickers)
-    
+
     indicators = {}
     for ticker in tickers:
         ticker = ticker.replace(".SA", "")
-        
+
+        # Get nested data
+        price = all_rsi[ticker]["price"]
+        variation = all_rsi[ticker]["variation"]
+        mme80_is_up = all_stochastic[ticker]["mme80_is_up"]
+        mm50_is_up = all_rsi[ticker]["mm50_is_up"]
+
+        # Delete unnecessary data
+        del all_rsi[ticker]["price"]
+        del all_rsi[ticker]["variation"]
+        del all_rsi[ticker]["mm50_is_up"]
+        del all_stochastic[ticker]["mme80_is_up"]
+        del all_stochastic[ticker]["price"]
+        del all_stochastic[ticker]["variation"]
+
         indicators[ticker] = {
-            "ifr2": all_rsi[ticker],
-            "stochastic": all_stochastic[ticker]
+            "price": price,
+            "variation": variation,
+            "mme80_is_up": mme80_is_up,
+            "mm50_is_up": mm50_is_up,
+            "rsi": all_rsi[ticker],
+            "stochastic": all_stochastic[ticker],
         }
-        
+
     return jsonify(indicators)
