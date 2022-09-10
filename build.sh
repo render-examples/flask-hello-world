2f4c9f3e5f295d17d15fdabddf4809ce58e18e18
Daniel Olagunju <todak2000@gmail.com>
2022-09-10 11:20:28 +0100

    fixing render deployment bug

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 # poetry install
-pip3 install -r requirements.txt
+pip install --upgrade pip
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
