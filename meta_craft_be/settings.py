f03c98b43b72cc56ad5562cc9e900ecaa17023ce
Daniel Olagunju <todak2000@gmail.com>
2022-09-10 10:46:36 +0100

    fixing python version

--- meta_craft_be/settings.py
+++ meta_craft_be/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-nzan7pebs3*07lcube77spd!r*s6#n1j%3thb#0bt#e17g8zt^"
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
