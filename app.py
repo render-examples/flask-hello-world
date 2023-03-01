975a9e1aa5c84c9766047dfb4612ab3c222a5ab2
Fred Scholldorf <alfred.scholldorf@globalrelay.net>
2023-03-01 15:19:45 -0500

    Update app.py
    add StartTime to output

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f'Hello, World!  StartTime={str(datetime.now())}'
