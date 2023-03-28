5245deecf3746982dc96623aa4b7cf1f55d38a84
Shubham Agarwal <shubh.mischieveous@gmail.com>
2023-03-29 00:01:04 +0530

    Update With Image

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Yukti!'
+    return '<img src="https://media.cnn.com/api/v1/images/stellar/prod/161004160803-05-sex-is-good-for-you-shutterstock-348997928.jpg?q=w_4148,h_2333,x_0,y_0,c_fill/w_1280" alt="Italian Trulli">
+'
