9f6e31d42a3f66b843cf266521270e382685baf7
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2023-04-16 23:23:01 -0400

    add index template

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
