662a0392b516ffac7b193afbebc29ad29bb07fda
BedrosovaYulia <BedrosovaYulia@gmail.com>
2023-05-10 13:27:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def notify():
         value = str(round(logs['event']['activity'][0]['value']))
 
         # create the text string
-        message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* \n{token_address}'
+        message = f'*Token transfer:*\n{txhash}\nfrom {from_address} \nto {to_address}: \nvalue: {value} *{token_symbol}* {token_address}'
         bot.send_message(chat_id=user_chat_id, text=message, parse_mode='MarkdownV2')
       
   return Response(status=200)
