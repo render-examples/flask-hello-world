379c5139e3a723b9c70961244cade3481249e0b9
camelPhi <mpylypov@gmail.com>
2023-06-18 18:51:16 -0400

    added some messages and styling to the placeholders

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ app = Flask(__name__)
 
 # Requires that "Less secure app access" be on
 # https://support.google.com/accounts/answer/6010255
-app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
-app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
-app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+# app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
+# app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
+# app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+app.config["MAIL_DEFAULT_SENDER"] = "GR Troop 15 Website <troop15@pylypovych.net>"
+app.config["MAIL_USERNAME"] = "troop15@pylypovych.net"
+app.config["MAIL_PASSWORD"] = "!Tr00p15!"
 app.config["MAIL_SERVER"] =   "mail.pylypovych.net"  # "smtp.gmail.com"
 app.config["MAIL_USE_TLS"] =  True
 app.config["MAIL_PORT"] =     587  #465  #587
