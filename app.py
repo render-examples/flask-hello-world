db2423d6c529fc9ffe186541fa505a79167c5fc3
I P <porteous@gmail.com>
2023-06-21 14:34:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def hyfproxy():
+def info():
     ticker = request.args.get('ticker')
     stock = yf.Ticker(ticker)
     info = stock.info
     return jsonify(info)
+
+@app.route('/news')
+def info():
+    ticker = request.args.get('ticker')
+    stock = yf.Ticker(ticker)
+    news = stock.news
+    return jsonify(news)
+
+
