2ffbea0581fd981b4a251509de3478e2fd7ee08e
I P <porteous@gmail.com>
2023-06-21 17:24:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import yfinance as yf
 from flask import Flask, request, jsonify
+import redis
+
+cache = redis.Redis(host='redis://red-ci9i4g59aq02iht9og6g', port=6379)
+redis_timeout = 900
 
 app = Flask(__name__)
 
@@ -17,4 +21,17 @@ def news():
     news = stock.news
     return jsonify(news)
 
-
+@app.route('/cache')
+def info():
+    ticker = request.args.get('ticker')
+    cached_info = cache.get(ticker)
+    if cached_info is not None:
+        cached_info['source'] = 'cached'
+        return jsonify(cached_info)
+    else:
+        stock = yf.Ticker(ticker)
+        info = stock.info
+        info['source'] = 'live'
+        cache.set(ticker, jsonify(info))
+        cache.expire(ticker, redis_timeout)
+        return jsonify(info)
