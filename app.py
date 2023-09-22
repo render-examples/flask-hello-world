ffa14bda28d6845908b6e69efa0ef5ef5300f0a5
jim7207 <jimhitch@gmail.com>
2023-09-22 08:13:12 -0500

    send Jim valid responses via SMS

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import json
 from flask import Flask,request, jsonify
+import os
 from twilio.twiml.messaging_response import MessagingResponse
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.environ.get('TWILIO_PHONE_NUMBER')
+TARGET_PHONE_NUMBER = os.environ.get('TARGET_PHONE_NUMBER')
+JIM_PHONE_NUMBER = os.environ.get('JIM_PHONE_NUMBER')
+
+from twilio.rest import Client
+
 
 responses = []
 
@@ -18,11 +28,20 @@ def webhook():
     # Start our TwiML response
     resp = MessagingResponse()
     if message_body == "1" or message_body == "2":
-        resp.message("Got it!")    
+        resp.message("Got it!") 
+        send_sms(message_body)   
     else: 
         resp.message("Please send 1 or 2. I was unable to record your message.") 
     
     return str(resp)
 
+def send_sms(message_body):
+    client = Client(TWILIO_ACCOUNT_SID,TWILIO_AUTH_TOKEN)
+    message = client.messages.create(
+        body="Got a response from James: " + message_body,
+        from_=TWILIO_PHONE_NUMBER,
+        to=JIM_PHONE_NUMBER
+    )
+    print(f"SMS sent to dad with message ID: {message.sid}")
 
 
