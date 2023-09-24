e9bd606773fc8c6f53579bf6d357801d70670f4f
john <60197372+john7373@users.noreply.github.com>
2023-09-24 14:55:52 +0300

    Update expfind.py

--- models/expfind.py
+++ models/expfind.py
@@ -1,30 +1,19 @@
 import openai
 import json
 
-
 # Chargez le fichier JSON local
-with open("C:\\Users\\7john\\OneDrive\\Documents\stage\\backend\\IAGORA-API\\data\\donnerExperts.json", "r") as file:
+with open("C:\\Users\\7john\\OneDrive\\Documents\\stage\\backend\\IAGORA-API\\data\\donnerExperts.json", "r") as file:
     developers = json.load(file)
-
-# Convertissez les données en une chaîne JSON
-json_data = json.dumps(developers)
-
-# Définissez le prompt avec la chaîne JSON
-prompt = f"""{json_data}\n\nRecherche des développeurs java avec plus de 3 ans d'expérience :"""
-
-# Demandez à GPT-3 de filtrer les développeurs
+    json_data = json.dumps(developers)
+# Appeler l'API GPT
 response = openai.Completion.create(
-    engine="davinci",
-    prompt=prompt,
-    max_tokens=1000
+    engine='text-davinci-003',
+    prompt = f"""{json_data}\n\nRecherche des développeurs angular avec plus de 3 ans d'expérience :""",
+    max_tokens= 10000
 )
 
-# Analysez la réponse pour extraire les développeurs filtrés
-filtered_developers = []
-data = response.choices[0].text.strip().split('\n')
-for entry in data:
-    expert = eval(entry)
-    if expert["specialization"] == "Java" and expert["years_of_experience"] > 3:
-        filtered_developers.append(expert)
+# Traiter la réponse
+answer = response.choices[0].text.strip()
 
-print(filtered_developers)
+# Afficher la réponse
+print(answer)
