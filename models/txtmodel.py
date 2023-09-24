b72a5100031f64c03d8f6397657ab3b1c616eba4
Nomena <razakamalala.nomena@gmail.com>
2023-09-24 17:50:44 +0300

    feature:add tutorat choices

--- models/txtmodel.py
+++ models/txtmodel.py
@@ -8,6 +8,15 @@ class txtmodel():
 
     def get_response(question):
         chat = ChatOpenAI()
+        conversation = ConversationChain(llm=chat)
 
-        conversation = ConversationChain(llm=chat)  
+        # Si l'étudiant a cliqué sur "Tuteur Virtuel"
+        if question.lower() == "virtual_tutor":
+            return "Vous avez choisi le Tuteur Virtuel. Comment puis-je vous aider davantage ?"
+
+        # Si l'étudiant a cliqué sur "Tuteur Réel"
+        elif question.lower() == "real_tutor":
+            return "Vous avez choisi le Tuteur Réel. Un tuteur réel sera disponible pour vous aider."
+
+        # Sinon, traiter la question normalement
         return conversation.run(question)
