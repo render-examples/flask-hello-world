12a37bc2f011daaa1b7ec315061b1636adc991a9
Azhr <azhr95@hotmail.com>
2023-11-30 22:26:26 -0500

    testing get requests

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello Azhr!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/trigger', methods=['GET'])
+def trigger_action():
+    # Code to perform the action when the button is pressed
+    # This could involve sending a signal to the ESP32
+    print("Button pressed!")
+    # Add code to communicate with the ESP32 here
+    return "Action triggered!"
