2215b2190e02f12e29ad2b1a66f453523ef9c3ce
Azhr <azhr95@hotmail.com>
2023-11-30 22:40:12 -0500

    button trial

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+flag = False
+
 
 @app.route('/')
 def index():
@@ -13,4 +15,14 @@ def trigger_action():
     # This could involve sending a signal to the ESP32
     print("Button pressed!")
     # Add code to communicate with the ESP32 here
-    return "Action triggered!"
+    if not flag:
+        return "Action not triggered!"
+    else:
+        flag = True
+        return "Action triggered!"
+
+
+@app.route('/buttonPressed')
+def buttonPressedAction():
+    flag = True
+    return render_template('index.html')
