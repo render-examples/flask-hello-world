c6951f6743f75b22a9942827d7222c55268593c0
Azhr <azhr95@hotmail.com>
2023-11-30 23:42:26 -0500

    adjusting the get method in trigger

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/trigger', methods=['GET'])
+@app.route('/trigger')
 def trigger_action():
     if flag:
         flag = False
