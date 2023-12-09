70b00dbcd9bf21a5058d54c25905db9c8fa4ce42
bvv-1 <fhinnoigsr@gmail.com>
2023-12-09 20:59:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     data = {'message': 'Hello, World!'}
     return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
