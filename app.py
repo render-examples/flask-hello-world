80bbf1850cb0bc38075f8af396c1aede78a67490
bvv-1 <fhinnoigsr@gmail.com>
2023-12-10 00:13:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ def hello_world():
     data = {'message': 'Hello, World!'}
     return jsonify(data)
 
+# GET /api/v1/points
+@app.route('/api/v1/points')
+def get_points():
+    data = {'points': 88}
+    return jsonify(data)
+
 if __name__ == '__main__':
     app.run()
