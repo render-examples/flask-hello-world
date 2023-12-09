55d152c651c6b011e4b68de0b91e6bd90ec91d36
bvv-1 <fhinnoigsr@gmail.com>
2023-12-10 00:38:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,74 @@ def hello_world():
     return jsonify(data), 200
 
 
+@app.route("/api/v1/plans/suggest", methods=['POST'])
+def suggest_plans():
+    """
+    API Endpoint: /api/v1/plans/suggest
+    HTTP Method: POST
+
+    Generate suggested daily plans based on the user's goal and tasks.
+
+    Request:
+    - Method: POST
+    - Headers:
+        Content-Type: application/json
+    - Body (JSON):
+        {
+            "goal": "computer",
+            "goal_points": 100,
+            "tasks": [
+                {"task": "cleaning", "award": 5},
+                {"task": "wash dishes", "award": 2}
+            ]
+        }
+
+    Response:
+    - Success (HTTP 200 OK):
+        {
+            "plans": [
+                {"day": 1, "plans_today": [{"task": "cleaning", "award": 5}, ...]},
+                {"day": 2, "plans_today": []},
+            ]
+        }
+    - Bad Request (HTTP 400 Bad Request):
+        {
+            "error": "Invalid data format"
+        }
+    """
+
+    try:
+        # POSTリクエストのボディからJSONデータを取得
+        request_data = request.get_json()
+
+        # 必要なデータが揃っているか確認
+        if 'goal' in request_data and 'goal_points' in request_data and 'tasks' in request_data:
+            goal = request_data['goal']
+            goal_points = request_data['goal_points']
+            tasks = request_data['tasks']
+
+            # プランを生成するロジック (ここでは簡略化)
+            plans = generate_daily_plans(goal, goal_points, tasks)
+
+            # 生成したプランを含むレスポンスを返す
+            return jsonify({"plans": plans}), 200
+        else:
+            # 必要なデータが見つからない場合はエラーメッセージを返す
+            return jsonify({"error": "Invalid data format"}), 400
+
+    except Exception as e:
+        # 例外が発生した場合はエラーメッセージを返す
+        return jsonify({"error": str(e)}), 500
+
+def generate_daily_plans(goal, goal_points, tasks):
+    # TODO: プラン生成のロジックを追加する
+    day1_plan = {"day": 1, "plans_today": [task for task in tasks]}
+    day2_plan = {"day": 2, "plans_today": []}
+    plans = [day1_plan, day2_plan]
+
+    return plans
+
+
 @app.route("/api/v1/plans/accept")
 def accept_plan():
     """
