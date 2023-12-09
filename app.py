c06d63f68d0a5fdcc7464db2148cd21c5e3f8ea2
bvv-1 <fhinnoigsr@gmail.com>
2023-12-09 20:03:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    data = {'message': 'Hello, World!'}
+    return jsonify(data)
